7b771c1aec38b28790f14f82e2ceb36bbed0e971
marcopollostat <marcopollostat@gmail.com>
2020-10-13 19:42:27 -0300

    First commit.

---
+++ code.R
@@ -0,0 +1,111 @@
+
+library(data.table)
+library(tidyverse)
+library(lubridate)
+
+d <- fread('gunzip -cq activity.zip')
+# set date variable as date format
+d$date <- as.Date(d$date)
+# See what structure of the object
+str(d)
+
+d[, month:= as.factor(month(d$date))]
+d[, day:= as.factor(day(d$date))]
+table(d$day)
+
+dl <- d[, sum(steps), by = day]
+ggplot(dl, aes(V1))+
+        geom_histogram()
+
+
+sl <- d[, c(lapply(.SD, sum, na.rm = FALSE)), .SDcols = c("steps"), by = .(date)] 
+ggplot(sl, aes(x = steps)) +
+        geom_histogram(fill = "blue", binwidth = 1000)
+
+
+
+###############################################################
+# check to see what proportion of the observations are missing
+###############################################################
+# count NA's in Steps variable
+sum(is.na(d$steps)) / dim(d)[1]
+# Compute percent of NA's in Steps variable
+mean(is.na(d$steps))
+# Compute percent of 0's in Steps variable
+sum(d$steps == 0, na.rm = TRUE) / dim(d)[1]
+mean(d$steps == 0, na.rm = TRUE) # this way, it computes mean without NA's, Change dimension.
+sum(d$steps > 0, na.rm = TRUE)
+# summary all variables of data set
+summary(d)
+
+# many plots
+par(mfrow = c(1,3))
+hist(d$steps)
+hist(log10(d$steps))
+plot(d$date, d$steps)
+
+# 1st day
+dia1 <- d[d$date == "2012-10-03",]
+sum(dia1$steps, na.rm = TRUE)
+
+table(month(d$date))
+month(d$date[15000])
+table(day(d$date))
+sum(month(d$date)==10)
+sum(month(d$date)==11)
+
+
+# total steps by day
+daily <- c()
+daily[0] <- 0
+day(d$date[0]) <- "01"
+#dim(d)[1]
+for (i in 290:556) {
+        #if(month(d$date)==10){
+                if(day(d$date[i]) == day(d$date[i-1])){
+                        #if(!is.na(d$steps)){
+                                daily[i] <- daily[i-1] + d$steps[i]
+                        #}
+                                                     
+                }
+        #}
+ print(daily)       
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
