891650a5b9f52af15b56e4f5380e69ba7c1b05a2
marcopollostat <marcopollostat@gmail.com>
2020-10-14 19:54:01 -0300

    New dataset from subsetting.

--- code.R
+++ code.R
@@ -1,20 +1,11 @@
 
 library(data.table)
 library(tidyverse)
-#library(lubridate)
+
 
 d <- fread('gunzip -cq activity.zip')
 # set date variable as date format
 d$date <- as.Date(d$date)
-
-#d[, month:= as.factor(month(d$date))]
-#d[, day:= as.factor(day(d$date))]
-#table(d$day)
-
-#dl <- d[, sum(steps), by = day]
-#ggplot(dl, aes(V1))+
-#        geom_histogram()
-
 
 sl <- d[, c(lapply(.SD, sum, na.rm = FALSE)), .SDcols = c("steps"), by = .(date)] 
 ggplot(sl, aes(x = steps)) +
@@ -27,38 +18,15 @@ int <- d[, c(lapply(.SD, mean, na.rm = TRUE)), .SDcols = c("steps"), by = .(inte
 ggplot(int, aes(x = interval, y = steps)) +
         geom_line()
 
-#int[steps == max(steps), .(max_interval = interval)]
 int[which.max(int$steps), 1]
 
-###############################################################
-# check to see what proportion of the observations are missing
-###############################################################
-# count NA's in Steps variable
-sum(is.na(d$steps)) / dim(d)[1]
-# Compute percent of NA's in Steps variable
-mean(is.na(d$steps))
-# Compute percent of 0's in Steps variable
-sum(d$steps == 0, na.rm = TRUE) / dim(d)[1]
-mean(d$steps == 0, na.rm = TRUE) # this way, it computes mean without NA's, Change dimension.
-sum(d$steps > 0, na.rm = TRUE)
-# summary all variables of data set
-summary(d)
+sum(is.na(d$steps))
 
-# many plots
-par(mfrow = c(1,3))
-hist(d$steps)
-hist(log10(d$steps))
-plot(d$date, d$steps)
+d[is.na(steps), "steps"] <- d[, c(lapply(.SD, mean, na.rm = TRUE)), .SDcols = c("steps")]
 
-# 1st day
-dia1 <- d[d$date == "2012-10-03",]
-sum(dia1$steps, na.rm = TRUE)
 
-table(month(d$date))
-month(d$date[15000])
-table(day(d$date))
-sum(month(d$date)==10)
-sum(month(d$date)==11)
+
+
 
 
 
