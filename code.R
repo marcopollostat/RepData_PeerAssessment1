79ef145b4b0342adc8fa45bb95e3ecea12ae1b92
marcopollostat <marcopollostat@gmail.com>
2020-10-22 13:20:21 -0300

    news

--- code.R
+++ code.R
@@ -37,5 +37,22 @@ totalSteps[, .(Mean_Steps = mean(steps), Median_Steps = median(steps))]
 
 
 
+# Just recreating activityDT from scratch then making the new factor variable. (No need to, just want to be clear on what the entire process is.) 
+d <- data.table::fread(input = "data/activity.csv")
+d[, date := as.POSIXct(date, format = "%Y-%m-%d")]
+
+d[, `Day of Week`:= weekdays(x = date)]
+d[grepl(pattern = "Monday|Tuesday|Wednesday|Thursday|Friday", x = `Day of Week`), "weekday or weekend"] <- "weekday"
+d[grepl(pattern = "Saturday|Sunday", x = `Day of Week`), "weekday or weekend"] <- "weekend"
+d[, `weekday or weekend` := as.factor(`weekday or weekend`)]
+head(d, 10)
+
+
+
+d[is.na(steps), "steps"] <- d[, c(lapply(.SD, median, na.rm = TRUE)), .SDcols = c("steps")]
+IntervalDT <- activityDT[, c(lapply(.SD, mean, na.rm = TRUE)), .SDcols = c("steps"), by = .(interval, `weekday or weekend`)] 
+ggplot(IntervalDT , aes(x = interval , y = steps, color=`weekday or weekend`)) + geom_line() + labs(title = "Avg. Daily Steps by Weektype", x = "Interval", y = "No. of Steps") + facet_wrap(~`weekday or weekend` , ncol = 1, nrow=2)
+
+
 
 
